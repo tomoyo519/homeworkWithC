014b5712c31a6c797a293705cbd7a232ee821dce
tomoyo519 <tomoyo519@naver.com>
2023-06-19 04:30:30 +0900

    zinbup

---
+++ zinbup.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int zinNum, zinbup, tmp;
+    scanf("%d %d", &zinNum, &zinbup);
+    long long zin = 1;
+    int idx = 0;
+    while(1){
+     
+        if(zinNum / zin > 0){
+            zin *= zinbup;
+            idx++;
+        }
+        else if(zinNum / zin  == 0 || zinNum < zinbup){
+            zin = zin / zinbup;
+            break;
+        }
+    }
+    
+
+
+    while(1){
+        tmp = (zinNum / zin);
+        if(tmp<10){
+            printf("%d", tmp);
+        }else if(tmp>=10){
+            printf("%c", tmp + 55);
+        }if(zin ==1){
+            break;
+        }
+        zin /= zinbup;
+    }
+}
